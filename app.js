cee0fd8ba7d0384cfdba4c0db639704e6beb4bab
jinga4x <jainigo@profile.es>
2017-08-14 15:26:14 +0200

    error handling

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const v1Api = require('./lib/routes/api/v1');
 const app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', v1Api);
 
