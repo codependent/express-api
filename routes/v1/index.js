26b1290654401dc10442b9f5f5aa7bdc7d908cdd
jinga4x <jainigo@profile.es>
2017-08-14 13:47:47 +0200

    cleanup

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const peopleRoute = require('./people');
-const departmentsRoute = require('./departments');
 
 var router = express.Router();
 
@@ -15,22 +14,18 @@ var options = {
     },
     basePath: '/api/v1',
   },
-  apis: ['routes/v1/departments.js','routes/v1/people.js'], // Path to the API docs
+  apis: ['routes/v1/people.js'], // Path to the API docs
 };
 
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
 var swaggerSpec = swaggerJSDoc(options);
-
-
 router.get('/v1/api-docs.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
 });
 
 router.use('/v1/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
 router.use('/v1', peopleRoute);
-router.use('/v1', departmentsRoute);
+
 
 module.exports = router;
